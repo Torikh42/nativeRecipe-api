02a5597757b4cb10f4f411b794f24b9d6411dbc9
Torikh <torikhnaser42@gmail.com>
2025-09-03 16:18:41 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
+import morgan from "morgan";
 import recipeRoutes from "./routes/recipe.routes";
 import authRoutes from "./routes/auth.routes";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Middleware
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
-// Logging Middleware Sederhana
-app.use((req, res, next) => {
-  console.log(`Request received: ${req.method} ${req.originalUrl}`);
-  next();
-});
-
-// Routes
 app.get("/", (req, res) => {
   res.send("Recipe API is running!");
 });
 
-// Gunakan prefix /api untuk semua rute resep
 app.use("/api/recipes", recipeRoutes);
 
-// Gunakan prefix /api untuk semua rute otentikasi
 app.use("/api/auth", authRoutes);
 
-// Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
